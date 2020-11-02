8c1785c1c1547b38cea439dc882ee88d3671da48
Jakub Plecháč <loxtox74@gmail.com>
2020-11-02 12:20:58 +0100

    settings.py put in more files

---
+++ backend/backend/files/development.py
@@ -0,0 +1,16 @@
+
+#define path to files
+ROOT_URLCONF = 'backend.urls'
+WSGI_APPLICATION = 'backend.wsgi.application'
+
+
+# Internationalization
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+
+# Static files 
+STATIC_URL = '/static/'
